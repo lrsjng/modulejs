255ef01a9cb02fe3d46b2154ffba36da1e9a7ae5
Lars Jung <lrsjng@gmail.com>
2020-07-25 21:50:16 +0200

    Update.

--- dist/modulejs.js
+++ dist/modulejs.js
@@ -1,4 +1,4 @@
-/*! modulejs v2.8.0 - https://larsjung.de/modulejs/ */
+/*! modulejs v2.9.0 - https://larsjung.de/modulejs/ */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
