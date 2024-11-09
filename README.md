7e45904f9d73fabbc80b0c2e9e760e919666a8e9
Lars Jung <lrsjng@gmail.com>
2024-11-09 23:19:06 +0100

    Update.

--- README.md
+++ README.md
@@ -1,15 +1,174 @@
 # modulejs
 
-[![license][license-img]][github] [![web][web-img]][web] [![github][github-img]][github] [![npm][npm-img]][npm]  
-[![version][npm-v-img]][npm] [![downloads][npm-dm-img]][npm]
+[![license][license-img]][github] [![github][github-img]][github] [![npm][npm-img]][npm]  
 
-Lightweight JavaScript module system.
+A lightweight JavaScript module system (only ~2kB minified). It is not a module loader, it triggers no file system lookups or HTTP requests. It simply helps organizing code in small, maintainable and easy to use modules. Modules respect and resolve dependencies, the syntax is very similar to that of [RequireJS](https://requirejs.org).
+
+
+## Usage
+
+Define a module without dependencies.
+```js
+modulejs.define('a', function () {
+    // do and return whatever you like
+    // ...
+    return {val: 1};
+});
+```
+
+Define a module with dependencies.
+```js
+modulejs.define('b', ['a'], function (a) {
+    // ...
+    return [a.val, a.val + 1];
+});
+```
+
+Define another module.
+```js
+modulejs.define('main', ['jquery', 'b'], function ($, b) {
+    // ...
+    return {
+        start: function () {
+            console.log(b);
+        }
+    };
+});
+```
+
+It's easy to register 3rd party objects.
+```js
+modulejs.define('modernizr', Modernizr);
+```
+
+But you need to be careful with 'objects' that actually are functions, wrap them in functions.
+```js
+modulejs.define('jquery', function () {
+    return jQuery;
+});
+```
+
+Finally require one of the defined modules and run some code (for example after all DOM content is loaded).
+```js
+document.addEventListener('DOMContentLoaded', function () {
+    var main = modulejs.require('main');
+    main.start();
+});
+```
+
+
+## API
+
+### define
+
+Defines a module through a constructor function. This function will only be called once when module is first required. The return value will be stored and returned whenever this module will be required.
+```js
+// id: string, fn: function  ->  undefined
+modulejs.define(id, fn)
+```
+
+Same as above but with dependencies that get resolved first and will be passed in as arguments to the constructor.
+```js
+// id: string, deps: array of strings, fn: function  ->  undefined
+modulejs.define(id, deps, fn)
+```
+
+Defines a module through an already existing object that gets returned whenever the module is required.
+```js
+// id: string, obj: object  ->  undefined
+modulejs.define(id, obj)
+```
+
+Same as above but with dependencies that get resolved first.
+```js
+// id: string, deps: array of strings, obj: object  ->  undefined
+modulejs.define(id, deps, obj)
+```
+
+### require
+
+Returns an already defined module. Creates it if necessary.
+```js
+// id: string  ->  object
+modulejs.require(id)
+```
+
+For testing purposes it's possible to provide mock instances for selected modules to override original module definitions.
+```js
+// id: string, mocks: object  ->  object
+modulejs.require(id, mocks)
+```
+
+for example:
+```js
+modulejs.require('b', {a: 'testing'})
+```
+
+will resolve a dependency `a` with the string `testing` instead of the real module.
+
+### state
+
+Returns an object that represents the current state of all modules.
+```js
+//  ->  object
+modulejs.state()
+```
+
+returns an object of the form:
+```js
+{
+    // ...
+    main: {
+        deps: ['jquery', 'b']
+        init: true
+        reqd: []
+        reqs: ['jquery', 'a', 'b']
+    }
+    // ...
+}
+```
+
+### log
+
+Returns a string representing module dependencies in a easy to read format. If `inv` is `true` it shows dependents for each module.
+```js
+// inv: boolean  ->  string
+modulejs.log(inv)
+```
+
+The result will show all dependencies (transitiv):
+```
+* a -> [  ]
+* b -> [ a ]
+* main -> [ jquery, a, b ]
+  modernizr -> [  ]
+* jquery -> [  ]
+```
+
+and if `inv` is `true` it will show all dependents (transitiv):
+```
+* a -> [ b, main ]
+* b -> [ main ]
+* main -> [  ]
+  modernizr -> [  ]
+* jquery -> [ main ]
+```
+
+a `*` indicates whether a module was already instantiated.
+
+### create
+
+Returns a fresh, private instances of `modulejs` with no definitions or instances.
+```js
+//  ->  modulejs
+modulejs.create()
+```
 
 
 ## License
 The MIT License (MIT)
 
-Copyright (c) 2020 Lars Jung (https://larsjung.de)
+Copyright (c) 2024 Lars Jung (https://larsjung.de)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -30,14 +189,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 
-[web]: https://larsjung.de/modulejs/
 [github]: https://github.com/lrsjng/modulejs
 [npm]: https://www.npmjs.org/package/modulejs
 
 [license-img]: https://img.shields.io/badge/license-MIT-a0a060.svg?style=flat-square
-[web-img]: https://img.shields.io/badge/web-larsjung.de/modulejs-a0a060.svg?style=flat-square
 [github-img]: https://img.shields.io/badge/github-lrsjng/modulejs-a0a060.svg?style=flat-square
 [npm-img]: https://img.shields.io/badge/npm-modulejs-a0a060.svg?style=flat-square
-
-[npm-v-img]: https://img.shields.io/npm/v/modulejs.svg?style=flat-square
-[npm-dm-img]: https://img.shields.io/npm/dm/modulejs.svg?style=flat-square
--- dist/modulejs.js
+++ dist/modulejs.js
@@ -1,4 +1,4 @@
-/*! modulejs v2.9.0 - https://larsjung.de/modulejs/ */
+/*! modulejs v2.10.0 - undefined */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -8,123 +8,39 @@
 		exports["modulejs"] = factory();
 	else
 		root["modulejs"] = factory();
-})((typeof self !== 'undefined' ? self : this), function() {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ([
+})((typeof self !== 'undefined' ? self : this), () => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var _require = __webpack_require__(1),
-    assert = _require.assert,
-    for_own = _require.for_own,
-    has = _require.has,
-    resolve = _require.resolve;
-
-var create = function create() {
+  assert = _require.assert,
+  for_own = _require.for_own,
+  has = _require.has,
+  resolve = _require.resolve;
+var _create = function create() {
   // Module definitions.
-  var definitions = {}; // Module instances.
+  var definitions = {};
 
-  var instances = {}; // Defines a module for `id: String`, optional `deps: Array[String]`,
+  // Module instances.
+  var instances = {};
+
+  // Defines a module for `id: String`, optional `deps: Array[String]`,
   // `def: mixed`.
-
   var define = function define(id, deps, def) {
     // sort arguments
     if (def === undefined) {
       var _ref = [[], deps];
       deps = _ref[0];
       def = _ref[1];
-    } // check arguments
-
-
+    }
+    // check arguments
     assert(typeof id === 'string', "id must be string: ".concat(id));
     assert(!has(definitions, id), "id already defined: ".concat(id));
-    assert(Array.isArray(deps), "deps must be array: ".concat(id)); // accept definition
+    assert(Array.isArray(deps), "deps must be array: ".concat(id));
 
+    // accept definition
     definitions[id] = {
       id: id,
       deps: deps,
@@ -132,17 +48,17 @@ var create = function create() {
         return def;
       }
     };
-  }; // Returns an instance for `id`. If a `mocks` object is given, it is
+  };
+
+  // Returns an instance for `id`. If a `mocks` object is given, it is
   // used to resolve the dependencies.
-
-
   var require = function require(id, mocks) {
     assert(typeof id === 'string', "id must be string: ".concat(id));
     return resolve(definitions, mocks || instances, id);
-  }; // Returns an object that holds infos about the current definitions
+  };
+
+  // Returns an object that holds infos about the current definitions
   // and dependencies.
-
-
   var state = function state() {
     var res = {};
     for_own(definitions, function (def, id) {
@@ -161,14 +77,15 @@ var create = function create() {
         if (res[id2].reqs.indexOf(id) >= 0) {
           inv.push(id2);
         }
-      }); // all inverse dependencies
+      });
 
+      // all inverse dependencies
       res[id].reqd = inv;
     });
     return res;
-  }; // Returns a string that displays module dependencies.
+  };
 
-
+  // Returns a string that displays module dependencies.
   var log = function log(inv) {
     var out = '\n';
     for_own(state(), function (st, id) {
@@ -177,9 +94,8 @@ var create = function create() {
     });
     return out;
   };
-
   return {
-    create: create,
+    create: _create,
     define: define,
     log: log,
     require: require,
@@ -188,34 +104,33 @@ var create = function create() {
     _i: instances
   };
 };
-
-module.exports = create();
+module.exports = _create();
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ ((module) => {
 
 // Throws an error if `expr` is falsy.
 var assert = function assert(expr, msg) {
   if (!expr) {
     throw new Error("[modulejs] ".concat(msg));
   }
-}; // Iterates over all own props of an object.
+};
 
-
+// Iterates over all own props of an object.
 var for_own = function for_own(x, fn) {
   Object.keys(x).forEach(function (k) {
     return fn(x[k], k);
   });
-}; // Short cut for `hasOwnProperty`.
+};
 
-
+// Short cut for `hasOwnProperty`.
 var has = function has(x, id) {
   return (x || {}).hasOwnProperty(id);
-}; // Returns an new array containing no duplicates. Preserves first occurence and
+};
+
+// Returns an new array containing no duplicates. Preserves first occurence and
 // order.
-
-
 var uniq = function uniq(x) {
   var cache = {};
   return x.filter(function (val) {
@@ -223,33 +138,35 @@ var uniq = function uniq(x) {
     cache[val] = 1;
     return res;
   });
-}; // Resolves `id` to an object for the given definitions `defs` and already
+};
+
+// Resolves `id` to an object for the given definitions `defs` and already
 // instantiated objects `insts`. Adds any new instances to `insts`. If `insts`
 // is null it only resolves dependency IDs.
 // `stack` is used internal to check for circular dependencies.
+var _resolve = function resolve(defs, insts, id, stack) {
+  var onlyDepIds = !insts;
 
-
-var resolve = function resolve(defs, insts, id, stack) {
-  var onlyDepIds = !insts; // if a module is required that was already created return that object
-
+  // if a module is required that was already created return that object
   if (!onlyDepIds && has(insts, id)) {
     return insts[id];
-  } // check if `id` is defined
+  }
 
-
+  // check if `id` is defined
   var def = defs[id];
-  assert(def, "id not defined: ".concat(id)); // copy resolve stack and add this `id`
+  assert(def, "id not defined: ".concat(id));
 
+  // copy resolve stack and add this `id`
   stack = (stack || []).slice();
-  stack.push(id); // if `onlyDepIds` this will hold the dependency-IDs, otherwise it
-  // will hold the dependency-objects
+  stack.push(id);
 
+  // if `onlyDepIds` this will hold the dependency-IDs, otherwise it
+  // will hold the dependency-objects
   var deps = [];
   def.deps.forEach(function (depId) {
     // check for circular dependencies
     assert(stack.indexOf(depId) < 0, "circular dependencies: ".concat(depId, " in ").concat(stack));
-    var depDeps = resolve(defs, insts, depId, stack);
-
+    var depDeps = _resolve(defs, insts, depId, stack);
     if (onlyDepIds) {
       deps = deps.concat(depDeps);
       deps.push(depId);
@@ -257,27 +174,60 @@ var resolve = function resolve(defs, insts, id, stack) {
       deps.push(depDeps);
     }
   });
-
   if (onlyDepIds) {
     // return only dependency-ids in correct order
     return uniq(deps);
-  } // create, memorize and return object
+  }
+
+  // create, memorize and return object
   // using def.fn(...deps) instead would cost ~120B uglified
-
-
   var obj = def.fn.apply(undefined, deps);
   insts[id] = obj;
   return obj;
 };
-
 module.exports = {
   assert: assert,
   for_own: for_own,
   has: has,
-  resolve: resolve,
+  resolve: _resolve,
   uniq: uniq
 };
 
 /***/ })
-/******/ ]);
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__(0);
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
 });
